dfb510963aa99cc2d83316994dcefd8a98a37c9c
Yann OREAL <yoreal@e1r8p13.42.fr>
2014-04-05 15:02:05 +0200

    debut html

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<html>
+	<head>
+	<style>
+			.lettrine		{color:white;
+							font-family: tahoma;
+							font-size: 50;}
+			.name_website	{color:#BDBDBD;
+							font-family: tahoma;
+							font-size: 30;}
+			.bgcol_body		{background-color: #D8D8D8;
+							width: 94%;
+							height: 78%;
+							margin: 0 auto;}
+	</style>
+
+	</head>
+<body>
+	<header>
+	<table height=12% width=100% bgcolor=#0B243B>
+			<tr>
+				<td><span class="lettrine">B</span><span class="name_website">ook</span><span class="lettrine">R</span><span class="name_website">ush</span><hr /></td>
+			</tr>
+	</table>
+		
+		
+	</header>
+	
+		<div  class="bgcol_body">
+
+
+		</div>
+	
+	<footer>
+			<table width="100%" height=10%>
+			<tr>
+				<td style="text-align: left">Rush00 - avril 2014</td>
+				<td style="text-align: right">&copy;grass-kw, &copy;yoreal.</td>
+			</tr>
+			</table>
+	</footer>
+
+</html>
