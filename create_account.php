7775d838d5d8c7647cbe2da3b47f193946f81cbe
Yann OREAL <yoreal@e1r8p10.42.fr>
2014-04-06 14:15:06 +0200

    3 pages : index, connection, creation de compte

--- create_account.php
+++ create_account.php
@@ -28,7 +28,6 @@ vertical-align: baseline;}
 							font-family: tahoma;
 							font-size: 20;
 							text-align: center;}
-
 			.lettrine		{color:white;
 							font-family: tahoma;
 							font-size: 50;}
@@ -48,6 +47,7 @@ vertical-align: baseline;}
 			.float_right	{float: right;}
 			.float_left		{float: left;}
 			.center			{text-align: center;}
+			.tahoma20		{font-family: tahoma;}
 			.tahoma12		{font-family: tahoma;
 							font-size: 12;}
 			.tahoma15		{font-family: tahoma;
@@ -57,25 +57,8 @@ vertical-align: baseline;}
 							height: 10%;
 							margin: 0 auto;}
 			.table_center	{margin: 0 auto;
-							font-family: tahoma;}
-			.category		{text-align: center;
-							color: #0B243B;
-
-										}
-			.menu			{margin: 0 auto;
-							width: 90%;
-							height: 8%;}
-			.degrade 		{background: linear-gradient(to bottom, #FE642E, rgba(0,255,0,0));}
-			.meilleures		{background-color: #D8D8D8;
-							margin-left: 10%;
-							margin-top: -32px;
-							width: 200px;
-							display: block;
-							z-index: 2;
-							padding: 10px 4px;
 							font-family: tahoma;
-							font-size: 20;}				
-
+							border: 2px}
 	</style>
 
 	</head>
@@ -90,33 +73,31 @@ vertical-align: baseline;}
 		
 		</header>
 			<div  class="bgcol_body">
-				<div class ="float_right"><a href="create_account.php"><input class="button" type="button" name="create_account" value="Cr&eacute;er un compte"></div></a>
-				<div class ="float_right"><a href="connection.php"><input class="button" type="button" name="connection" value="Connectez-vous"></div></a>
-				<div class ="clear"></div>
+				<div class ="float_right"><a href="index.php"><input class="button" type="submit" name="create_account" value="Accueil"></a></div>
+				<br /><div class ="clear"></div>
+								
 				<br />
-				<br />
+				<form method="GET" action="./index.php">
+					<table class="table_center">
+						<tr>
+							<td colspan="2"><H1>Cr&eacute;ation de compte</H1><br /></td>
+						</tr>
+							<td colspan="2"><div class ="tahoma12"><a href="connection.php">D&eacute;j&agrave; client ? Connectez-vous.</a></div></td>
+						</tr>
+						<tr>
+							<td>Identifiant</td>
+							<td><input type=text name="login"</td>
+						</tr>
+						<tr>
+							<td>Mot de passe</td>
+							<td><input type=text name="passwd"</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type="submit" name="submit" value="OK"></td>
+						</tr>
+					</table>
 
-
-<!--------------------------------------RECHERCHE DANS LES CATEGORIES -------------------------------------->
-				<table class="menu">
-					<tr>
-					</tr>
-					<tr class="degrade">
-						<td class="category" "tahoma12">Litt&eacute;rature</td>
-						<td class="category" "tahoma12">Art & culure</td>
-						<td class="category" "tahoma12">Vie pratique</td>
-						<td class="category" "tahoma12">Cuisine</td>
-						<td class="category" "tahoma12">Tourisme</td>
-					</tr>
-
-				</table>
-<!--------------------------------------FIN RECHERCHE DANS LES CATEGORIES-------------------------------------->
-			<br /><br />
-			 <div>
-			 	<hr color="#0B243B">
-			 	<div class="meilleures">Les meilleures ventes</div>
-			 </div>
-			
 
 
 
